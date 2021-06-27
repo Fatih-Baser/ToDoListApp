a43356c1706395894b8c14cd9998b6a8d081da8b
fatih40 <baserfatih40@gmail.com>
2021-06-27 10:48:59 +0200

    Continous

---
+++ app/src/main/java/com/fatihbasertr/todolistapp/adapter/ToDoDiffUtil.kt
@@ -0,0 +1,29 @@
+package com.fatihbasertr.todolistapp.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.fatihbasertr.todolistapp.models.ToDoListDataModel
+
+class ToDoDiffUtil(
+    private val oldList: List<ToDoListDataModel>,
+    private val newList: List<ToDoListDataModel>
+): DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+                && oldList[oldItemPosition].title == newList[newItemPosition].title
+                && oldList[oldItemPosition].description == newList[newItemPosition].description
+                && oldList[oldItemPosition].priority == newList[newItemPosition].priority
+    }
+}
